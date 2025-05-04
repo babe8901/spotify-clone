90618b5fdfce30efad1403fd31194077c14d5335
Shubham Yadav <luckyshubham16@gmail.com>
2025-05-04 14:19:12 +0530

    Implemented 2FA

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDTO } from '../users/dto/create-user-dto';
 import { User } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 import { LoginDTO } from './dto/login-dto';
+import { JwtAuthGuard } from './jwt-auth-guard';
+import { Enable2FAType } from './types';
+import { ValidateTokenDTO } from './dto/validate-token.dto';
+import { UpdateResult } from 'typeorm';
 
 @Controller('auth')
 export class AuthController {
@@ -25,5 +36,42 @@ export class AuthController {
     loginDTO: LoginDTO,
   ) {
     return this.authService.login(loginDTO);
+  }
+
+  @Get('enable-2fa')
+  @UseGuards(JwtAuthGuard)
+  enable2FA(
+    @Request()
+    req: {
+      user: { userId: number };
+    },
+  ): Promise<Enable2FAType> {
+    console.log(req.user);
+    return this.authService.enable2FA(req.user.userId);
+  }
+
+  @Get('disable-2fa')
+  @UseGuards(JwtAuthGuard)
+  disable2FA(
+    @Request()
+    req: {
+      user: { userId: number };
+    },
+  ): Promise<UpdateResult> {
+    return this.authService.disable2FA(req.user.userId);
+  }
+
+  @Post('validate-2fa')
+  @UseGuards(JwtAuthGuard)
+  validate2FA(
+    @Request()
+    req: { user: { userId: number } },
+    @Body()
+    ValidateTokenDTO: ValidateTokenDTO,
+  ): Promise<{ verified: boolean }> {
+    return this.authService.validate2FAToken(
+      req.user.userId,
+      ValidateTokenDTO.token,
+    );
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,7 +4,8 @@ import { LoginDTO } from './dto/login-dto';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { ArtistsService } from 'src/artists/artists.service';
-import { PayloadType } from './types';
+import { Enable2FAType, PayloadType } from './types';
+import * as speakeasy from 'speakeasy';
 
 @Injectable()
 export class AuthService {
@@ -14,7 +15,11 @@ export class AuthService {
     private artistService: ArtistsService,
   ) {}
 
-  async login(loginDTO: LoginDTO): Promise<{ accessToken: string }> {
+  async login(
+    loginDTO: LoginDTO,
+  ): Promise<
+    { accessToken: string } | { validate2FA: string; message: string }
+  > {
     const user = await this.userService.findOne(loginDTO);
     const passwordMatched = await bcrypt.compare(
       loginDTO.password,
@@ -31,9 +36,62 @@ export class AuthService {
         payload.artistId = artist.id;
       }
 
+      if (user.enable2FA && user.twoFASecret) {
+        return {
+          validate2FA: 'http://localhost:3000/auth/validate-2fa',
+          message:
+            'Please send the one time password/token from your Authenticator app',
+        };
+      }
+
       return { accessToken: this.jwtService.sign(payload) };
     } else {
       throw new UnauthorizedException('Password does not match');
+    }
+  }
+
+  async enable2FA(userId: number): Promise<Enable2FAType> {
+    const user = await this.userService.findById(userId);
+
+    if (!user) {
+      throw new Error('Invalid user id');
+    }
+
+    if (user.enable2FA && user.twoFASecret) {
+      return { secret: user.twoFASecret };
+    }
+
+    const secret = speakeasy.generateSecret();
+    console.log(secret);
+    user.twoFASecret = secret.base32;
+    await this.userService.updateSecretKey(user.id, user.twoFASecret);
+    return { secret: user.twoFASecret };
+  }
+
+  async disable2FA(userId: number) {
+    const user = await this.userService.findById(userId);
+
+    if (!user) {
+      throw new Error('Invalid user id');
+    }
+
+    return await this.userService.disable2FA(user.id);
+  }
+
+  async validate2FAToken(userId: number, token: string) {
+    try {
+      const user = await this.userService.findById(userId);
+      if (!user || !user.twoFASecret) {
+        throw new Error('Invalid user id or secret');
+      }
+      const verified = speakeasy.totp.verify({
+        secret: user.twoFASecret,
+        token: token,
+        encoding: 'base32',
+      });
+      return { verified };
+    } catch {
+      throw new UnauthorizedException('Error verifying token');
     }
   }
 }
